0d94f1d4d136975852ab3a4f417d43379ea5c198
Adeoluwa Opadina <atopadina@gmail.com>
2026-01-25 11:47:41 -0500

    helm yaml files and more...

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,23 +1,22 @@
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = var.cluster_name
-
+  cluster_name = var.cluster_name
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # Lets EKS create the API endpoint; keep public access ON for now (simpler)
   cluster_endpoint_public_access = true
-
   enable_irsa = true
 
-  # Managed node group (autoscaling)
   eks_managed_node_groups = {
     default = {
       name           = "ng-default"
-      instance_types = ["t3.small"]
+      instance_types = ["t3.small"
+      ]
 
       min_size     = 1
       desired_size = 1
@@ -25,7 +24,23 @@ module "eks" {
     }
   }
 
+  # ✅ Correct modern EKS access control
+  access_entries = {
+    dev_user = {
+      principal_arn = "arn:aws:iam::381492260180:user/T_DEV"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
   tags = {
-    "Project" = "tech-challenge-2"
+    Project = "tech-challenge-2"
   }
 }
